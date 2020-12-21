ddf6e4487162f33fb9ccf40c6ab916e5154cdd67
Henry Ning <henryhuning@gmail.com>
2020-12-21 16:09:25 +0000

    32. Multiple If Statements in Succession 9min

--- main.py
+++ main.py
@@ -1,14 +1,27 @@
-# 🚨 Don't change the code below 👇
-year = int(input("Which year do you want to check? "))
-# 🚨 Don't change the code above 👆
+print("Welcome to the rollercoaster!")
+height = int(input("What is your height in cm? "))
+bill = 0
 
-#Write your code below this line 👇
+if height >= 120:
+  print("You can ride the rollercoaster!")
+  age = int(input("What is your age? "))
+  if age < 12:
+    bill = 5
+    print("Child tickets are $5.")
+  elif age <= 18:
+    bill = 7
+    print("Youth tickets are $7.")
+  elif age >= 45 and age <= 55:
+    print("Everything is going to be ok. Have a free ride on us!")
+  else:
+    bill = 12
+    print("Adult tickets are $12.")
+  
+  wants_photo = input("Do you want a photo taken? Y or N. ")
+  if wants_photo == "Y":
+    bill += 3
+  
+  print(f"Your final bill is ${bill}")
 
-if ((year % 4) != 0):
-  print("not leap")
-elif year%100 !=0:
-  print("leap")
-elif year % 400 !=0:
-  print("not leap")
 else:
-  print("leap")
+  print("Sorry, you have to grow taller before you can ride.")
