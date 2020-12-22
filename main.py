516fd50529bf68f573222120b81607d40fd86e91
Henry Ning <henryhuning@gmail.com>
2020-12-22 20:09:20 +0000

    33. [Interactive Coding Exercise] Pizza Order Practice 6min

--- main.py
+++ main.py
@@ -1,27 +1,34 @@
-print("Welcome to the rollercoaster!")
-height = int(input("What is your height in cm? "))
+# 🚨 Don't change the code below 👇
+print("Welcome to Python Pizza Deliveries!")
+size = input("What size pizza do you want? S, M, or L ")
+add_pepperoni = input("Do you want pepperoni? Y or N ")
+extra_cheese = input("Do you want extra cheese? Y or N ")
+# 🚨 Don't change the code above 👆
+
+#Write your code below this line 👇
+
 bill = 0
 
-if height >= 120:
-  print("You can ride the rollercoaster!")
-  age = int(input("What is your age? "))
-  if age < 12:
-    bill = 5
-    print("Child tickets are $5.")
-  elif age <= 18:
-    bill = 7
-    print("Youth tickets are $7.")
-  elif age >= 45 and age <= 55:
-    print("Everything is going to be ok. Have a free ride on us!")
-  else:
-    bill = 12
-    print("Adult tickets are $12.")
-  
-  wants_photo = input("Do you want a photo taken? Y or N. ")
-  if wants_photo == "Y":
-    bill += 3
-  
-  print(f"Your final bill is ${bill}")
+if size=="S":
+  bill = 15
+  if add_pepperoni=="Y":
+    bill += 2
 
-else:
-  print("Sorry, you have to grow taller before you can ride.")
+if size=="M":
+  bill = 20
+  if add_pepperoni=="Y":
+    bill += 3
+
+if size=="L":
+  bill = 25
+  if add_pepperoni=="Y":
+    bill += 3
+
+if extra_cheese=="Y":
+  bill += 1
+
+print(bill)
+
+
+
+
