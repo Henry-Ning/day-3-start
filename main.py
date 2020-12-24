03b671a013e7d1e3d5db64b199b8021b590fc18e
Henry Ning <henryhuning@gmail.com>
2020-12-24 17:04:58 +0000

    35. [Interactive Coding Exercise] Love Calculator 14minv

--- main.py
+++ main.py
@@ -1,37 +1,24 @@
 # 🚨 Don't change the code below 👇
-print("Welcome to Python Pizza Deliveries!")
-size = input("What size pizza do you want? S, M, or L ")
-add_pepperoni = input("Do you want pepperoni? Y or N ")
-extra_cheese = input("Do you want extra cheese? Y or N ")
+print("Welcome to the Love Calculator!")
+name1 = input("What is your name? \n")
+name2 = input("What is their name? \n")
 # 🚨 Don't change the code above 👆
 
 #Write your code below this line 👇
 
-bill = 0
 
-if size=="S":
-  bill = 15
-  if add_pepperoni=="Y":
-    bill += 2
+name1_lower = name1.lower()
+name2_lower = name2.lower()
+true_count = name1_lower.count('t') + name1_lower.count('r')+ name1_lower.count('u') + name1_lower.count('e') + name2_lower.count('t') + name2_lower.count('r')+ name2_lower.count('u') + name2_lower.count('e') 
+love_count = name1_lower.count('l') + name1_lower.count('o')+ name1_lower.count('v') + name1_lower.count('e') + name2_lower.count('l') + name2_lower.count('o')+ name2_lower.count('v') + name2_lower.count('e') 
+print(true_count)
+print(love_count)
+love_score = int(f"{true_count}" + f"{love_count}")
+print(love_score)
 
-if size=="M":
-  bill = 20
-  if add_pepperoni=="Y":
-    bill += 3
-
-if size=="L":
-  bill = 25
-  if add_pepperoni=="Y":
-    bill += 3
-
-if extra_cheese=="Y":
-  bill += 1
-
-print(bill)
-
-age=46
-if age>=45 and age<=55:
-  print("free")
-
-
-
+if love_score < 10 or love_score > 90:
+  print(f"Your score is {love_score}, you go together like coke and mentos")
+elif love_score > 40 and love_score < 50:
+  print(f"Your love is {love_score}, you are alright together.")
+else:
+  print(f"Your score is {love_score}")
